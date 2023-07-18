340dff3a4cbb164c1f0c33a9a8e79678a136be87
xiaoluoHe <sevnape@outlook.com>
2023-07-18 21:10:05 +0800

    fix: call directly of toString function

--- packages/vutils/src/common/isType.ts
+++ packages/vutils/src/common/isType.ts
@@ -4,6 +4,6 @@
  * @param type type
  * @returns
  */
-const isType = (value: any, type: string): boolean => toString.call(value) === `[object ${type}]`;
+const isType = (value: any, type: string): boolean => Object.prototype.toString.call(value) === `[object ${type}]`;
 
 export default isType;
