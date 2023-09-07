37e8a0bc72074fd659fb9d8c8fe742fb5d4cd631
xiaoluoHe <sevnape@outlook.com>
2023-09-07 13:42:02 +0800

    refactor: remove textMeasure test code

--- packages/vutils/__tests__/text/textMeasure.test.ts
+++ packages/vutils/__tests__/text/textMeasure.test.ts
@@ -1,4 +1,6 @@
+import type { TextMeasureMethod } from '../../src';
 import { TextMeasure } from '../../src';
+import { TestTextMeasure } from './util';
 
 describe('downgrade', () => {
   it('test without vrender', () => {
@@ -7,5 +9,15 @@ describe('downgrade', () => {
     const { width: width1 } = textMeasure.measureWithNaiveCanvas(str);
     const { width: width2 } = textMeasure.fullMeasure(str);
     expect(width2).toBe(width1);
+  });
+});
+
+describe('downgrade 1', () => {
+  it('test without vrender', () => {
+    const textMeasureTest = new TestTextMeasure({});
+    const testMethod = ['canvas', 'vrender'] as TextMeasureMethod[];
+    const { report } = textMeasureTest.test(testMethod, undefined);
+    const { canvas, vrender } = report;
+    expect(canvas.errMean.height).toEqual(vrender.errMean.height);
   });
 });
