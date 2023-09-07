8d4663214ff40a361da2c1d0a032640d1818aba2
lixuefei.1313 <lixuef1313@163.com>
2023-09-07 11:34:14 +0800

    feat: support precision math of add & sub

---
+++ packages/vutils/__tests__/math/math.test.ts
@@ -0,0 +1,10 @@
+import { precisionAdd, precisionSub } from '../../src/math';
+
+describe('precision', () => {
+  it('precision add', () => {
+    expect(precisionAdd(0.1, 0.2)).toBe(0.3);
+  });
+  it('precision sub', () => {
+    expect(precisionSub(33, 23.33)).toBe(9.67);
+  });
+});
