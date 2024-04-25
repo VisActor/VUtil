c05026647e2950646d1b3486d0345984aca08130
xile611 <dingling112@gmail.com>
2024-04-25 19:05:22 +0800

    feat: add bounds util `calculateAnchorOfBounds`

---
+++ packages/vutils/src/graphics/bounds-util.ts
@@ -0,0 +1,64 @@
+import type { IBoundsLike } from 'src/data-structure';
+
+export type InsideBoundsAnchorType = 'inside-top' | 'inside-bottom' | 'inside-left' | 'inside-right';
+
+export type BoundsAnchorType =
+  | 'top'
+  | 'bottom'
+  | 'left'
+  | 'right'
+  | 'top-right'
+  | 'top-left'
+  | 'bottom-right'
+  | 'bottom-left'
+  | 'center';
+
+export const calculateAnchorOfBounds = (bounds: IBoundsLike, anchorType: BoundsAnchorType | InsideBoundsAnchorType) => {
+  const { x1, x2, y1, y2 } = bounds;
+  const rectWidth = Math.abs(x2 - x1);
+  const rectHeight = Math.abs(y2 - y1);
+  let anchorX = (x1 + x2) / 2;
+  let anchorY = (y1 + y2) / 2;
+
+  let sx = 0;
+  let sy = 0;
+
+  switch (anchorType) {
+    case 'top':
+    case 'inside-top':
+      sy = -0.5;
+      break;
+    case 'bottom':
+    case 'inside-bottom':
+      sy = 0.5;
+      break;
+    case 'left':
+    case 'inside-left':
+      sx = -0.5;
+      break;
+    case 'right':
+    case 'inside-right':
+      sx = 0.5;
+      break;
+    case 'top-right':
+      sx = 0.5;
+      sy = -0.5;
+      break;
+    case 'top-left':
+      sx = -0.5;
+      sy = -0.5;
+      break;
+    case 'bottom-right':
+      sx = 0.5;
+      sy = 0.5;
+      break;
+    case 'bottom-left':
+      sx = -0.5;
+      sy = 0.5;
+  }
+
+  anchorX += sx * rectWidth;
+  anchorY += sy * rectHeight;
+
+  return { x: anchorX, y: anchorY };
+};
