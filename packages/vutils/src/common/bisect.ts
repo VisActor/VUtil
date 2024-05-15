ffd3ad577cf2048ae1ea9ae6ed15d2be561a0de0
xile611 <dingling112@gmail.com>
2024-05-15 19:11:23 +0800

    fix: revert `bisect`

--- packages/vutils/src/common/bisect.ts
+++ packages/vutils/src/common/bisect.ts
@@ -5,8 +5,15 @@ export function bisect(a: number[], x: number, lo: number = 0, hi?: number) {
   if (isNil(hi)) {
     hi = a.length;
   }
-
-  return binaryFuzzySearchInNumberRange(lo, hi, mid => ascending(a[mid], x));
+  while (lo < (hi as number)) {
+    const mid: number = (lo + (hi as number)) >>> 1;
+    if (ascending(a[mid], x) > 0) {
+      hi = mid;
+    } else {
+      lo = mid + 1;
+    }
+  }
+  return lo;
 }
 
 /**
