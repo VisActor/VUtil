e6dd62fc1a664eb9d16d96080a808e12402d466e
xiaoluoHe <sevnape@outlook.com>
2024-09-24 17:57:59 +0800

    fix: incorrect result of obb bounds intersection

--- packages/vutils/__tests__/graphics/bounds-util.test.ts
+++ packages/vutils/__tests__/graphics/bounds-util.test.ts
@@ -1,4 +1,4 @@
-import { calculateAnchorOfBounds, Bounds } from '../../src';
+import { calculateAnchorOfBounds, Bounds, OBBBounds } from '../../src';
 
 describe('bounds utils', () => {
   it('calculateAnchorOfBounds', () => {
@@ -20,5 +20,16 @@ describe('bounds utils', () => {
     expect(calculateAnchorOfBounds(bounds, 'inside-bottom')).toEqual({ x: 75, y: 100 });
     expect(calculateAnchorOfBounds(bounds, 'inside-left')).toEqual({ x: 50, y: 75 });
     expect(calculateAnchorOfBounds(bounds, 'inside-right')).toEqual({ x: 100, y: 75 });
+  });
+});
+
+describe('OBB intersect', () => {
+  it('intersects', () => {
+    const a = new OBBBounds();
+    const b = new OBBBounds();
+    a.setValue(-2.644351612437859, 28.00798797607422, 41.37162433971058, 40.00798797607422, 1.5707963267948966);
+    b.setValue(21.90184229070489, 29.27997589111329, 68.46179407293147, 41.27997589111329, 1.5707963267948966);
+
+    expect(a.intersects(b)).toBe(false);
   });
 });
