f732e98e363255cb9dcda8c184e3f90880c600af
xiaoluoHe <sevnape@outlook.com>
2025-04-03 11:24:57 +0800

    feat: add unit test case

--- packages/vutils/__tests__/data-structure/obb.test.ts
+++ packages/vutils/__tests__/data-structure/obb.test.ts
@@ -1,4 +1,4 @@
-import { OBBBounds } from '../../src/index';
+import { IOBBBounds, OBBBounds, obbSeparation } from '../../src/index';
 
 describe('OBBBounds', () => {
   it('clone of OBBBounds', () => {
@@ -11,5 +11,23 @@ describe('OBBBounds', () => {
     expect(a.y1).toBe(b.y1);
     expect(a.y2).toBe(b.y2);
     expect(a.angle).toBe(b.angle);
+  });
+
+  it('distance of OBBBounds (simulation for xAxis label)', () => {
+    const a = new OBBBounds();
+    a.setValue(0, 0, 100, 20, Math.PI / 4);
+    const b = new OBBBounds();
+    b.setValue(0, 0, 100, 20, Math.PI / 4);
+    b.translate(100, 0);
+    expect(obbSeparation(a as any as IOBBBounds, b as any as IOBBBounds)).toBeCloseTo(100 / Math.sqrt(2) - 20);
+  });
+
+  it('distance of OBBBounds (simulation for yAxis labels', () => {
+    const a = new OBBBounds();
+    a.setValue(0, 0, 100, 20, Math.PI / 6);
+    const b = new OBBBounds();
+    b.setValue(0, 0, 100, 20, Math.PI / 6);
+    b.translate(0, 100);
+    expect(obbSeparation(a as any as IOBBBounds, b as any as IOBBBounds)).toBeCloseTo((100 / 2) * Math.sqrt(3) - 20);
   });
 });
