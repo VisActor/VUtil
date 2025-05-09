f931e77925bf78ace22d0dc46c82c8b372e55fa1
xiaoluoHe <sevnape@outlook.com>
2025-05-09 14:49:12 +0800

    fix: export more types for imagecloud

--- packages/vlayouts/src/imagecloud/index.ts
+++ packages/vlayouts/src/imagecloud/index.ts
@@ -2,4 +2,4 @@ export { transform as imagecloudTransform } from './imagecloud';
 export { GridLayout as ImageCloudGridLayout } from './layout/grid/grid';
 export { StackLayout as ImageCloudStackLayout } from './layout/stack';
 export { SpiralLayout as ImageCloudSpiralLayout } from './layout/spiral';
-export type { ImageCloudOptions } from './interface';
+export type * from './interface';
