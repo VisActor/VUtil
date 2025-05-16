5c8743463b999cd8d6d357be5f4f9bf7fbb3278b
xile611 <dingling112@gmail.com>
2025-05-16 11:19:56 +0800

    fix: export utils and interface of venn

--- packages/vlayouts/src/venn/index.ts
+++ packages/vlayouts/src/venn/index.ts
@@ -1,6 +1,8 @@
 import { transform, transformMark } from './venn';
 
 export * from './interface';
+export * from './utils/interface';
+export * from './utils/path';
 
 export const vennTransform = transform;
 export const vennMarkTransform = transformMark;
