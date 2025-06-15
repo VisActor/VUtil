990870a2ecaeefb95ef4fd0d7ad8024109632352
szxc <shuzhuxvchuang@163.com>
2025-06-15 22:17:01 +0800

    test: improve tests for 2 and 1 element venn diagrams with empty sets

--- packages/vlayouts/__tests__/venn/venn.test.ts
+++ packages/vlayouts/__tests__/venn/venn.test.ts
@@ -89,15 +89,41 @@ test('Data transform of 2 element venn with empty set', async () => {
   expect((result[0] as IVennCircleDatum).y).toBeCloseTo(250, 0);
 
   const circles = getCirclesFromArcs(getArcsFromPath((result[3] as IVennOverlapDatum).path));
-  const expectresult = result.slice(1, 2).reverse() as IVennCircleDatum[];
+  const circleA = result[2] as IVennCircleDatum;
+  const circleB = result[1] as IVennCircleDatum;
 
   expect(circles.length).toEqual(2);
 
-  expect(circles[0].radius).toBeCloseTo(expectresult[0].radius, 0);
-  expect(circles[0].x).toBeCloseTo(expectresult[0].x, 0);
-  expect(circles[0].y).toBeCloseTo(expectresult[0].y, 0);
+  expect(circles[0].radius).toBeCloseTo(circleA.radius, 0);
+  expect(circles[0].x).toBeCloseTo(circleA.x, 0);
+  expect(circles[0].y).toBeCloseTo(circleA.y, 0);
 
-  expect(circles[1].radius).toBeCloseTo(expectresult[1].radius, 0);
-  expect(circles[1].x).toBeCloseTo(expectresult[1].x, 0);
-  expect(circles[1].y).toBeCloseTo(expectresult[1].y, 0);
+  expect(circles[1].radius).toBeCloseTo(circleB.radius, 0);
+  expect(circles[1].x).toBeCloseTo(circleB.x, 0);
+  expect(circles[1].y).toBeCloseTo(circleB.y, 0);
+});
+
+test('Data transform of 1 element venn with empty set and no overlaps', async () => {
+  const result = await transform(
+    {
+      x0: 0,
+      y0: 0,
+      x1: 500,
+      y1: 500
+    },
+    data2.slice(0, 2)
+  );
+
+  expect(result.length).toEqual(2);
+
+  expect(result[0].type).toEqual('circle');
+  expect(result[1].type).toEqual('circle');
+
+  expect((result[0] as IVennCircleDatum).radius).toBeCloseTo(250, 0);
+  expect((result[0] as IVennCircleDatum).x).toBeCloseTo(250, 0);
+  expect((result[0] as IVennCircleDatum).y).toBeCloseTo(250, 0);
+
+  expect((result[1] as IVennCircleDatum).radius).toBeCloseTo((result[0] as IVennCircleDatum).radius, 0);
+  expect((result[1] as IVennCircleDatum).x).toBeCloseTo((result[0] as IVennCircleDatum).x, 0);
+  expect((result[1] as IVennCircleDatum).y).toBeCloseTo((result[0] as IVennCircleDatum).y, 0);
 });
